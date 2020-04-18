545b14ff0a82455880991e6a00abc0b6ffa9b128
AbrahamGarcia22 <A01023256@itesm.mx>
2020-04-17 23:14:06 -0500

    final

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,5 +1,4 @@
 #include "Matrices.cpp"
-#include "Grafe.cpp"
 int main(){
     Matrices m;
     int size;
@@ -12,15 +11,4 @@ int main(){
     }
     m.multiplicacionMatriz(dimensiones,size);
     cout<<'-'*90<<endl;
-    Grafe g;
-
-    bool mat[N][N] = {
-      {0, 1, 0, 3, 1},
-      {0, 0, 1, 3, 0},
-      {0, 3, 0, 4, 5},
-      {1, 1, 1, 0, 4},
-      {0, 1, 2, 0, 0},
-   };
-   g.hamCycle(mat);
-   return 0;
 }
